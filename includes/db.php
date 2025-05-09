db6bad25365ca31d0e7211d30813a72fbf96e0ca
jeroen weij <jeroenweij88@gmail.com>
2025-05-09 21:31:03 +0200

    Get db variables from env vars

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,9 @@
 <?php
+include_once('.env.php');
 try {
     // Create the PDO connection
-    $pdo = new PDO('mysql:host=host;port=port;dbname=mis;charset=utf8', 'username', 'password');
+    $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME;
+    $pdo = new PDO($dsn, DB_USER, DB_PASSWORD);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     // Handle connection errors
